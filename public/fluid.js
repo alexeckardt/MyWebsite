6b466fc76028cfe043da2f36a0574b54bb0b6f31
Alex Eckardt <realalexeck@gmail.com>
2023-12-09 18:38:45 -0500

    Card

--- public/fluid.js
+++ public/fluid.js
@@ -937,7 +937,7 @@ let bloomFramebuffers = [];
 let sunrays;
 let sunraysTemp;
 
-let ditheringTexture = createTextureAsync('/public/images/LDR_LLL1_0.png');
+let ditheringTexture = createTextureAsync('/images/LDR_LLL1_0.png');
 
 const blurProgram            = new Program(blurVertexShader, blurShader);
 const copyProgram            = new Program(baseVertexShader, copyShader);
