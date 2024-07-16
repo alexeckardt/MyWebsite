ae0f1368c1171be4feb296150eaa7a83038eb057
Alex Eckardt <realalexeck@gmail.com>
2024-07-15 23:32:01 -0400

    Cards

--- src/App.jsx
+++ src/App.jsx
@@ -24,24 +24,58 @@ function App() {
             <MainProjectEmph 
                 title='Soulflame'
                 subheader='Action Adventure Platformer'
+                desc="A Game I've been passionately developing for 3 years, but currently on a hiatus do focus on my schooling."
                 image={'soulflame_bkg.png'}
             >
-            A Game I've been passionately developing for 3 years, but currently on a hiatus do focus on my schooling.
+                <Button 
+                    size="small" 
+                    href='https://alexeckardt.itch.io/soulflamealpha' 
+                    target='_blank'>
+                      Learn More
+                  </Button>
+                  <Button 
+                    size="small" 
+                    href='https://instagram.com/soulflamegame' 
+                    target='_blank'>
+                      Showcase
+                  </Button>
             </MainProjectEmph>
 
             <MainProjectEmph 
                 title='Pinball Pioneer'
                 subheader='Top 10 Puzzle Game of GMTK 2023 Game Jam'
+                desc="Pinball Pioneer is a puzzle game, originally developed in 48 hours for the largest game jam in (at the time) Itch.io history. 
+                      After a week-long voting period, Pinball Pioneer placed 7th place out of over 7000 submissions."
+                image={'pinball_bkg.png'}
             >
-            Pinball Pioneer is a puzzle game, originally developed in 48 hours for the largest game jam in (at the time) Itch.io history. 
-            After a week-long voting period, Pinball Pioneer placed 7th place out of over 7000 submissions.
+                <Button 
+                    size="small" 
+                    href='https://itch.io/jam/gmtk-2023/rate/2159023' 
+                    target='_blank'>
+                      Learn More
+                  </Button>
+                  <Button 
+                    size="small" 
+                    href='https://itch.io/jam/gmtk-2023' 
+                    target='_blank'>
+                      About GTMK Jam
+                  </Button>
             </MainProjectEmph>
 
             <MainProjectEmph 
                 title='GO Transit Viewer'
                 subheader='GTA Transit Network Visualizer'
+                desc='GO Transit view is a visualizer of all Bus & Train routes issued by GO Transit in Sothern Ontario.'
+                image={'gotransitviewer.png'}
+                imageX={40}
+                // imageY={5}
             >
-            GO Transit view is a visualizer of all Bus & Train routes issued by GO Transit in Sothern Ontario.
+                <Button 
+                    size="small" 
+                    href='https://alexeckardt.github.io/GO-Transit-Visualizer/' 
+                    target='_blank'>
+                      Try out
+                  </Button>
             </MainProjectEmph>
       
       </Section>
--- src/Components/MainProjectEmph.tsx
+++ src/Components/MainProjectEmph.tsx
@@ -1,35 +1,66 @@
 
 import { Box, Button, Card, CardActions, CardContent, styled, Typography } from '@mui/material';
-import React from 'react';
+import React, { CSSProperties } from 'react';
 
 
 type ProjectEmphProps = {
     title: string,
     subheader?: string,
+    desc?: string,
     children: React.ReactNode,
     image?: string,
-    imageAlt?: string
+    imageAlt?: string,
+    
+    imageX?: number,
+    imageY?: number
 };
 
-const FloatingCardContent = styled(CardContent)({
-    boxSizing: 'border-box'
-});
-
 const ColumnBox = styled(Box)({
+    display: 'flex',
     float: 'left',
-    width: '50%',
+    flexFlow: 'row nowrap',
+    height: '100%',
+    // border: '1px solid red'
 });
 
 const MainProjectEmph = (props: ProjectEmphProps) => {
+
+
+    const imageWidth = '80px'
+    const imageGap = '20px'
+
+    const imageX = (props.imageX) ? props.imageX : 0;
+    const imageY = (props.imageY) ? props.imageY : 0;
+
+    const imageLineStyle: CSSProperties = {
+        position:'absolute',
+
+        // backgroundColor: 'black', 
+        width: imageWidth, 
+        height: '230px', 
+        
+        overflow: 'hidden', 
+        top: '0', 
+        left: '0',
+
+        backgroundImage: `url(${props.image})`,
+        backgroundSize: 'cover',
+
+        backgroundPosition: `${imageX}% ${imageY}%`,
+        // WebkitBackgroundSize: '100%'
+        
+    }
+
     return (
-        <Card style={{ marginTop: '20px' }}>
-            <FloatingCardContent>
-                <ColumnBox style={{backgroundColor: 'black', width: '30px', height: '1fr', overflow: 'hidden', position:'relative', top:0}}>
-                    <img src={props.image} alt={props.imageAlt} style={{margin: 'auto'}}/>
+        <Card style={{ marginTop: '20px', width: '100%'}}>
+            <CardContent style={{position: 'relative', paddingLeft: `calc(${imageWidth} + ${imageGap})`}}>
+                <ColumnBox style={imageLineStyle}>
+                    {/* <img src={props.image} alt={props.imageAlt} style={{objectFit: 'fill'}}/> */}
                 </ColumnBox>
-                <ColumnBox>
+
+                <ColumnBox style={{width: '90%'}}>
                     <Box>
-                        <Typography variant="h5" component="div">
+                        <Typography variant="h5" fontWeight='bold' component="div">
                             {props.title}
                         </Typography>
                         <Typography variant="body2" color="secondary">
@@ -39,16 +70,21 @@ const MainProjectEmph = (props: ProjectEmphProps) => {
                         <br></br>
 
                         {/* Body */}
-                        <Typography variant="body2" color="text.secondary" sx={{ width: '80%' }}>
-                            {props.children}
+                        <Typography variant="body2" color="text.secondary" sx={{ width: '100%' }}>
+                            {props.desc}
                         </Typography>
 
                         <CardActions>
-                            <Button size="small">Learn More</Button>
+                            {props.children}
                         </CardActions>
                     </Box>
                 </ColumnBox>
-            </ FloatingCardContent>
+
+                <ColumnBox>
+                Hello.
+                </ColumnBox>
+
+            </ CardContent>
             
         </Card>
     );
