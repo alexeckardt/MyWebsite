920f11e4c91dc78b78048ecb3c1ae1a87e1c6fd0
Alex Eckardt <alex.eckardt@samos.com>
2025-06-15 22:42:59 -0400

    Finish Dark Mode

--- src/app/Background.tsx
+++
@@ -1,7 +0,0 @@
-import Image from "next/image"
-
-export const Background = () => {
-    return (
-        <div className="h-64 w-full overflow-hidden bg-[url(/images/backgrounds/header_dark.png)] bg-bottom"></div>
-    )
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,24 @@ import { Welcome } from "./Sections/Welcome";
 import { WorkExperience } from "./Sections/WorkExperience";
 import { Projects } from "./Sections/Projects";
 import { Experiments } from "./Sections/Experiments";
-import { Background } from "./Background";
+import { SocialIcon } from "@/components/social_icon";
 
 export default function Home() {
   return (
-      <div className="items-center justify-items-center min-h-screen sm:m-10 mb-20 xl:mx-30 gap-16 bg-royal-50 dark:bg-royal-950 shadow-xl sm:rounded-lg overflow-hidden">
+    <div className="items-center justify-items-center min-h-screen sm:m-10 mb-20 xl:mx-30 gap-16 bg-royal-50 dark:bg-royal-950 shadow-xl sm:rounded-lg overflow-hidden">
 
-        <Background />
+      <Welcome />
+      <WorkExperience />
+      <Projects />
+      <Experiments />
 
-        <Welcome />
-
-
-
-        <WorkExperience />
-        <Projects />
-        <Experiments />
-
+      <div className="py-25 flex flex-col font-funnel gap-2 items-center justify-center text-center">
+        CV
+        <SocialIcon website="/Alexander_Eckardt_Website_Resume.pdf">
+          <svg xmlns="http://www.w3.org/2000/svg" className='w-9 h-9' viewBox="0 1 24 24"><path fill="currentColor" fill-rule="evenodd" d="M14.25 2.5a.25.25 0 0 0-.25-.25H7A2.75 2.75 0 0 0 4.25 5v14A2.75 2.75 0 0 0 7 21.75h10A2.75 2.75 0 0 0 19.75 19V9.147a.25.25 0 0 0-.25-.25H15a.75.75 0 0 1-.75-.75zm.75 9.75a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1 0-1.5zm0 4a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1 0-1.5z" clip-rule="evenodd" /><path fill="currentColor" d="M15.75 2.824c0-.184.193-.301.336-.186q.182.147.323.342l3.013 4.197c.068.096-.006.22-.124.22H16a.25.25 0 0 1-.25-.25z" /></svg>
+        </SocialIcon>
       </div>
+
+    </div>
   );
 }
