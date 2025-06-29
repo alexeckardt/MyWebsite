b95b0f8105ad815d419b5146583c2e88a4ead8ec
Alex Eckardt <alex.eckardt@samos.com>
2025-06-29 19:11:35 -0400

    Simplfied Project descriptions

--- src/app/Sections/Projects.tsx
+++ src/app/Sections/Projects.tsx
@@ -19,15 +19,15 @@ export const Projects = () => {
                     >
 
                         <div className="font-funnel dark:text-royal-300 flex flex-col gap-y-2 mb-2">
-                            <div>
+                            {/* <div>
                                 This was my final end-of-degree project, collaborated with some of the smartest people I know.
-                            </div>
+                            </div> */}
                             <div>
-                                MeetingBot is an open-source meeting bot API that integrates with online meeting platforms like Zoom, Microsoft Teams, and Google Meet. Essentially, the meeting bots can join video calls and record audio, which can then be accessed programmatically through an API.
+                                MeetingBot is an open-source meeting bot API that integrates with online meeting platforms like Zoom, Microsoft Teams, and Google Meet. Bots can join video calls and record audio, which can then be accessed programmatically through an API.
                             </div>
-                            <div>
+                            {/* <div>
                                 This project was a huge learning experience for me, as it was my first time working with a larger team of developers on a long-term project. I was responsible for Bot Development, as well as creating an example application.
-                            </div>
+                            </div> */}
                         </div>
                     </Project>
 
@@ -38,23 +38,14 @@ export const Projects = () => {
                         year={2024}
                         bgImage={'pinball_highres.png'}
                         backgroundPositionY='35%'
-
                         itchHref="https://alexeckardt.itch.io/pinball-pioneer"
                     >
                         <div className="font-funnel dark:text-royal-300 flex flex-col gap-y-2 mb-2">
                             <div>
-                                Pinball Pioneer is a puzzle game, originally developed in 48 hours for the largest game jam in (at the time) Itch.io history.
-                            </div>
-                            <div>
-
-                                The game was developed for the Game Maker's Tool Kit (GMTK) Game Jam in July 2023. The theme of the game jam was 'Roles Reversed' -- meaning that some aspect of the game was to be backwards in some way. After a week-long voting period, Pinball Pioneer finished Top 10 (7th) out of 7000 submissions.
-                            </div>
-                            <div>
-                                Play as a little worker inside a pinball machine who's job is to design the board! Obviously, if the pinball-player wins the course then the pinball machine is too easy-- and the machine loses tokens! Use whatever you have available to you and make sure that the balls don't go into the holes!
+                                Pinball Pioneer is a puzzle game developed in 48 hours for the GMTK Game Jam in July 2023, where it finished Top 10 (7th) out of 7000 submissions, allowing players to design a pinball machine while ensuring the game remains challenging.
                             </div>
                         </div>
                     </Project>
-
                     <Project
                         name={"Soulflame (Alpha)"}
                         tagline={"Solo-developed Action Adventure Game"}
@@ -66,13 +57,7 @@ export const Projects = () => {
                     >
                         <div className="font-funnel dark:text-royal-300 flex flex-col gap-y-2 mb-2">
                             <div>
-                                A Game I've been passionately developing for 3 years, but currently on a hiatus do focus on my schooling.
-                            </div>
-                            <div>
-                                The game boasts a captivating Metroidvania-style gameplay, allowing players to freely explore an expansive 2D environment. As players delve into the game, they will navigate through different areas teeming with enemies, while unraveling the mysteries of the main charachters backstory.
-                            </div>
-                            <div className='italic'>
-                                "When Keira Eldrid arrives on the Old Continent, she finds herself in the midst of calamity: the return of ancient titans known as the Spirit Phoenixes. To prevent the destruction of the continent, she'll have to harness the power of new weapons and abilities by connecting with her inner self."
+                                Soulflame is a Metroidvania-style game that I've been developing for 4+ years, currently on hiatus, featuring an expansive 2D environment where players unravel the main character's backstory amidst the return of ancient evils.
                             </div>
                         </div>
                     </Project>
@@ -89,12 +74,6 @@ export const Projects = () => {
                         <div className="font-funnel dark:text-royal-300 flex flex-col gap-y-2 mb-2">
                             <div>
                                 GO Transit Viewer is an interactive visual tool I developed to map all bus and train routes operated by GO Transit across Southern Ontario. My goal was to create an intuitive and user-friendly way for people to explore the region's extensive transit network.
-                            </div>
-                            <div>
-                                Using JavaScript and HTML, I built the viewer by leveraging the same public data sources utilized by companies like Google and Apple. This allowed me to create a geographically accurate representation of GO Transit's network, ensuring both precision and reliability in how routes and connections are displayed.
-                            </div>
-                            <div>
-                                I'm very proud of how this prototype turned out, and hope to revisit this project again in the future and utilize what I've learned since making this first version.
                             </div>
                         </div>
                     </Project >
